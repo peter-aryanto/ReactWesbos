6cb709e3b56b836261c371aa01bb6aa0c2aa8b10
Peter Aryanto <peter.aryanto@email.com>
2022-06-28 19:24:33 +1000

    header-order-inventory

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import Reach from 'react';
 import styled from 'styled-components';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
 
 const Myp = styled.p`
   color: red;
 `;
 
-export default function () {
+export default function App() {
   return (
+    <>
+    <Myp>Hello Again!</Myp>
     <div className='catch-of-the-day'>
-      <Myp>Heeey</Myp>
+      <div className='menu'>
+        <Header />
+      </div>
+      <Order />
+      <Inventory />
     </div>
+    </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+    <header className='top'>
+      <h1>Catch of the Day</h1>
+      <h3 className='tagline'>
+        <span>Fresh Daily</span>
+      </h3>
+    </header>
+    </>
+  );
+}
