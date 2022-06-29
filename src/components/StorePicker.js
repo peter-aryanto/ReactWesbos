15c7cb267eb24a5cafe0195b6463faf3c9edfdee
Peter Aryanto <peter.aryanto@email.com>
2022-06-30 01:20:10 +1000

    event and navigate

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function StorePicker() {
+  //const storeNameInput = React.createRef();  
+  const storeNameInput = useRef();  
+  const navigate = useNavigate();
+  const goToStore = (e) => {
+  //const goToStore = useCallback((e) => {
+    e.preventDefault();
+    console.log(storeNameInput);
+    console.log(storeNameInput.current);
+    console.log(storeNameInput.current.value);
+    navigate(`store/${storeNameInput.current.value}`);
+  }
+  //}, []);
   return (
-    <form className='store-selector'>
+    <form className='store-selector' onSubmit={goToStore}>
       <h2>Please Enter A Store</h2>
-      <input type='text' required placeholder='Store Name' />
+      <input type='text' ref={storeNameInput} placeholder='Store Name' defaultValue='My Store' />
       <button type='submit'>Visit Store →</button>
     </form>
   );
