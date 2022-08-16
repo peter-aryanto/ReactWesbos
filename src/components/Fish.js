13111c926b7c831d7c368fd9192c93d00fa5aab1
Peter Aryanto <peter.aryanto@email.com>
2022-08-16 13:56:15 +1000

    available vs sold out

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,13 +4,14 @@ import { formatCurrency } from '../helpers';
 export default function Fish(poops) {
   const fish = poops.details;
   const { desc, price } = fish;
+  const isAvailable = fish.status === 'available';
   return (
     <li className='menu-fish'>
       <h3 className='single-fish'>🐟 {fish.name}</h3>
       <img src={fish.image} alt={fish.name} width={50} height={50} />
       <p>{desc}</p>
       <div className='price'>{formatCurrency(price)}</div>
-      <button>Add to Cart</button>
+      <button disabled={!isAvailable}>{isAvailable?'Add to Cart':'Sold Out'}</button>
     </li>
   );
 }
