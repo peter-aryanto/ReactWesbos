cb99b8b2fbff62471947afc012cacc8200e5a531
Peter Aryanto <peter.aryanto@email.com>
2022-08-17 18:35:49 +1000

    unavailable order

--- src/components/Order.js
+++ src/components/Order.js
@@ -4,11 +4,25 @@ import { formatCurrency } from '../helpers';
 export default function Order(poops) {
   const orderKeys = Object.keys(poops.order);
   const renderOrderItem = (key) => {
-    return (
-      <li>
-        {key}
-      </li>
-    );
+    const fish = poops.fishes[key];
+    if (!fish)
+      return;
+
+    const isAvailable = fish.status === 'available';
+    if (isAvailable) {
+      const count = poops.order[key];
+      return (
+        <li key={key}>
+          {count} {fish.name}
+          {formatCurrency(count * fish.price)}
+        </li>
+      );
+    }
+    else {
+      return (
+        <li key={key}>Sorry {fish.name} is no longer available.</li>
+      );
+    }
   }
   const totalPrice = orderKeys.reduce(
     (currentTotal, key) => {
